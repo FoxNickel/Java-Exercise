6b26fbeb0afa0aa1499068ba3f93980669532a4e
HarleyYu <hly49366499@gmail.com>
2016-04-18 22:18:31 +0800

    Add annotation

--- src/com/moneymanager/Login.java
+++ src/com/moneymanager/Login.java
@@ -10,16 +10,17 @@ import javax.swing.JTextField;
 public class Login extends JFrame{
 	public Login(){
 		super();
-		setTitle("欢迎使用个人理财账本");
-		setSize(400,270);
-		setLocationRelativeTo(null);
-		getContentPane().setLayout(null);
-		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);//主窗口
+		setTitle("欢迎使用个人理财账本");//窗口标题
+		setSize(400,270);//设置窗口宽度和高度
+		setLocationRelativeTo(null);//设置窗口居中
+		getContentPane().setLayout(null);//Layout为空
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);//将组件显示出来
+		//主窗口
 		
 		JLabel welcome = new JLabel();
 		welcome.setText("欢迎使用个人理财系统");
-		welcome.setBounds(80, 10, 300, 20);
-		welcome.setFont(new Font(Font.DIALOG,Font.BOLD,20));
+		welcome.setBounds(80, 10, 300, 20);//坐标，宽高
+		welcome.setFont(new Font(Font.DIALOG,Font.BOLD,20));//字体设置（字体类型，加粗，大小）
 		getContentPane().add(welcome);//文本框（显示欢迎提示）
 		
 		JLabel userName = new JLabel();
@@ -41,23 +42,21 @@ public class Login extends JFrame{
 		getContentPane().add(passWord);//文本框（显示密码两个字）
 		
 		JPasswordField passWordInput = new JPasswordField();
-		passWordInput.setHorizontalAlignment(JTextField.CENTER);
+		passWordInput.setHorizontalAlignment(JTextField.CENTER);//输入内容居中
 		passWordInput.setBounds(130,95,200,30);
 		passWordInput.setFont(new Font(Font.DIALOG,Font.BOLD,20));
-		passWordInput.setEchoChar('*');
+		passWordInput.setEchoChar('*');//输入后看到的是*
 		getContentPane().add(passWordInput);//输入框（输入密码）
 		
 		final JButton login = new JButton();
 		login.setText("登陆");
 		login.setContentAreaFilled(false);//按钮无颜色
-		getContentPane().add(login);
 		login.setBounds(40,155,130,40);
 		getContentPane().add(login);//登陆按钮
 		
 		final JButton exit = new JButton();
 		exit.setText("退出");
 		exit.setContentAreaFilled(false);//按钮无颜色
-		getContentPane().add(login);
 		exit.setBounds(195,155,135,40);
 		getContentPane().add(exit);//退出按钮
 		
